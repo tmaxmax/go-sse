e0def608c53c2bb5dd110c1c38d2efdbef2d1d94
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-08-14 11:09:41 +0300

    Add documentation comment

--- sse/event/field/count_writer.go
+++ sse/event/field/count_writer.go
@@ -2,14 +2,15 @@ package field
 
 import "io"
 
+// countWriter is a proxy that counts the bytes written to the wrapped writer.
 type countWriter struct {
 	count int
 	w     io.Writer
 }
 
-func (c *countWriter) Write(b []byte) (n int, err error) {
-	n, err = c.w.Write(b)
+func (c *countWriter) Write(b []byte) (int, error) {
+	n, err := c.w.Write(b)
 	c.count += n
 
-	return
+	return n, err
 }
