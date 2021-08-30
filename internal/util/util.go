96a711e270689b27b208a3b915b5a71bdeebc8dd
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-08-30 13:23:46 +0300

    Create utility for converting string to bytes using unsafe

--- internal/util/util.go
+++ internal/util/util.go
@@ -1,6 +1,10 @@
 package util
 
-import "strings"
+import (
+	"reflect"
+	"strings"
+	"unsafe"
+)
 
 func EscapeNewlines(s string) string {
 	return strings.ReplaceAll(strings.ReplaceAll(s, "\n", "\\n"), "\r", "\\r")
@@ -8,4 +12,10 @@ func EscapeNewlines(s string) string {
 
 func IsNewlineChar(b byte) bool {
 	return b == '\n' || b == '\r'
+}
+
+func Bytes(s string) []byte {
+	return (*[0x7fff0000]byte)(unsafe.Pointer(
+		(*reflect.StringHeader)(unsafe.Pointer(&s)).Data),
+	)[:len(s):len(s)]
 }
