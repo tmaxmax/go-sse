2621ea890a1c9542ed8e0809318409e9c4416db6
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-08-30 13:49:50 +0300

    Remove Retry field allocation on write

--- server/event/retry.go
+++ server/event/retry.go
@@ -9,13 +9,21 @@ import (
 
 // Retry is a field that tells the client to set the event stream reconnection time to
 // the number of milliseconds it provides.
-type Retry time.Duration
+func Retry(t time.Duration) RetryField {
+	return RetryField{buf: strconv.AppendInt(nil, t.Milliseconds(), 10)}
+}
 
-func (r Retry) name() parser.FieldName {
+// RetryField holds the representation in bytes of a retry field. This way when writing an Event
+// the duration isn't converted to bytes on each write and no allocations are made.
+type RetryField struct {
+	buf []byte
+}
+
+func (r RetryField) name() parser.FieldName {
 	return parser.FieldNameRetry
 }
 
-func (r Retry) apply(e *Event) {
+func (r RetryField) apply(e *Event) {
 	if e.retryIndex == -1 {
 		e.retryIndex = len(e.fields)
 		e.fields = append(e.fields, r)
@@ -24,6 +32,6 @@ func (r Retry) apply(e *Event) {
 	}
 }
 
-func (r Retry) repr() []byte {
-	return strconv.AppendInt(nil, time.Duration(r).Milliseconds(), 10)
+func (r RetryField) repr() []byte {
+	return r.buf
 }
