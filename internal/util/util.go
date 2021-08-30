c9dbfdd99cb94e48bfd645102f8255a342ab79dc
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-08-30 12:00:28 +0300

    Create utility function for newline char comparator

--- internal/util/util.go
+++ internal/util/util.go
@@ -5,3 +5,7 @@ import "strings"
 func EscapeNewlines(s string) string {
 	return strings.ReplaceAll(strings.ReplaceAll(s, "\n", "\\n"), "\r", "\\r")
 }
+
+func IsNewlineChar(b byte) bool {
+	return b == '\n' || b == '\r'
+}
