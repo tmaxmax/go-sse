32effd139a2e98ee222040351c8284d40fb26eaf
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-08-31 15:13:24 +0300

    Improve field documentation

--- server/event/id.go
+++ server/event/id.go
@@ -7,8 +7,9 @@ import (
 
 // ID is an event field that sets the event's id.
 // It takes a string as its value, given that IDs can be of any value.
+// If an ID has a null byte inside its value is ignored by the client.
 //
-// Use strconv.Itoa if you want to use integer IDs.
+// Use functions from the fmt or strconv packages if you want to use numbers for ID values.
 //
 // An ID cannot have multiple lines, ensure it does not contain any newline characters (\n or \r).
 // The library does not do any checks so if the condition is not met the protocol will be broken.
