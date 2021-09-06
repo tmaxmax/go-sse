b1b1d3f8415e654dcd7da35703c8c88118205b06
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-09-06 15:30:59 +0300

    Extract validators and write documentation

--- client/default.go
+++ client/default.go
@@ -6,20 +6,32 @@ import (
 	"time"
 )
 
+// The DefaultValidator is the default response validation function. It checks the content type to be
+// text/event-stream and the response status code to be 200 OK.
+var DefaultValidator ResponseValidator = func(r *http.Response) error {
+	if r.StatusCode != http.StatusOK {
+		return fmt.Errorf("expected status code %d, received %d", http.StatusOK, r.StatusCode)
+	}
+	if rc, ec := r.Header.Get("Content-Type"), "text/event-stream"; rc != ec {
+		return fmt.Errorf("expected content type %q, received %q", ec, rc)
+	}
+	return nil
+}
+
+// NoopValidator is a validator function that treats all responses as valid.
+var NoopValidator ResponseValidator = func(_ *http.Response) error {
+	return nil
+}
+
+// DefaultClient is the client that is used by the free functions exported by this package.
+// Unset properties on new clients are replaced with the ones set for the default client.
 var DefaultClient = &Client{
 	HTTPClient:              http.DefaultClient,
 	DefaultReconnectionTime: time.Second * 5,
-	ResponseValidator: func(r *http.Response) error {
-		if r.StatusCode != http.StatusOK {
-			return fmt.Errorf("expected status code %d, received %d", http.StatusOK, r.StatusCode)
-		}
-		if rc, ec := r.Header.Get("Content-Type"), "text/event-stream"; rc != ec {
-			return fmt.Errorf("expected content type %q, received %q", ec, rc)
-		}
-		return nil
-	},
+	ResponseValidator:       DefaultValidator,
 }
 
+// NewConnection creates a connection using the default client.
 func NewConnection(r *http.Request) *Connection {
 	return DefaultClient.NewConnection(r)
 }
