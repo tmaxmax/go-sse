49c7aea1fc5d17fa725724ef39fc3b55196a0451
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-09-06 15:27:15 +0300

    Update documentation

--- server/event/field.go
+++ server/event/field.go
@@ -98,14 +98,13 @@ func Comment(comment string) Field {
 	}
 }
 
-// ID is an event field that sets the event's id.
-// It takes a string as its value, given that IDs can be of any value.
-// If an ID has a null byte inside its value is ignored by the client.
+//ID is an event field that sets the event's id.
+//It takes a string as its value, given that IDs can be of any value.
+//If an ID has a null byte inside its value is ignored by the client.
 //
-// Use functions from the fmt or strconv packages if you want to use numbers for ID values.
+//Use functions from the fmt or strconv packages if you want to use numbers for ID values.
 //
-// An ID cannot have multiple lines, ensure it does not contain any newline characters (\n or \r).
-// The library does not do any checks so if the condition is not met the protocol will be broken.
+//An ID cannot have multiple lines. If it has, the program will panic.
 func ID(id string) Field {
 	return Field{
 		nameBytes:  fieldBytes[parser.FieldNameID],
@@ -116,7 +115,7 @@ func ID(id string) Field {
 
 // Name is the event field that sets the event's type.
 //
-// A Name cannot have multiple lines. Make sure this condition is met or the protocol will be broken.
+// A Name cannot have multiple lines. If it has, the program will panic.
 func Name(name string) Field {
 	return Field{
 		nameBytes:  fieldBytes[parser.FieldNameEvent],
