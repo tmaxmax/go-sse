c439249a9252ca6b21d8ae60877a99a241d5f5f6
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-09-18 15:49:51 +0300

    joe: Test for callback errors and closing subs to multiple topics

--- joe_test.go
+++ joe_test.go
@@ -34,6 +34,8 @@ func (m *mockReplayProvider) GC() error {
 var _ sse.ReplayProvider = (*mockReplayProvider)(nil)
 
 func TestNewJoe(t *testing.T) {
+	t.Parallel()
+
 	rp := &mockReplayProvider{errGC: errors.New("")}
 	j := sse.NewJoe(sse.JoeConfig{
 		ReplayProvider:   rp,
@@ -59,6 +61,8 @@ func TestNewJoe(t *testing.T) {
 }
 
 func TestJoe_Stop(t *testing.T) {
+	t.Parallel()
+
 	rp := &mockReplayProvider{errGC: errors.New("")}
 	j := sse.NewJoe(sse.JoeConfig{
 		ReplayProvider: rp,
@@ -80,10 +84,12 @@ func TestJoe_Stop(t *testing.T) {
 	})
 }
 
-// TODO: Fix tests
-
-func subscribe(t testing.TB, p sse.Provider, ctx context.Context) <-chan []*sse.Message {
+func subscribe(t testing.TB, p sse.Provider, ctx context.Context, topics ...string) <-chan []*sse.Message {
 	t.Helper()
+
+	if len(topics) == 0 {
+		topics = []string{sse.DefaultTopic}
+	}
 
 	ch := make(chan []*sse.Message, 1)
 
@@ -97,7 +103,7 @@ func subscribe(t testing.TB, p sse.Provider, ctx context.Context) <-chan []*sse.
 			return nil
 		}
 
-		_ = p.Subscribe(ctx, sse.Subscription{Callback: fn})
+		_ = p.Subscribe(ctx, sse.Subscription{Callback: fn, Topics: topics})
 
 		ch <- msgs
 	}()
@@ -105,37 +111,138 @@ func subscribe(t testing.TB, p sse.Provider, ctx context.Context) <-chan []*sse.
 	return ch
 }
 
+type mockContext struct {
+	context.Context
+	waitingOnDone chan struct{}
+}
+
+func (m *mockContext) Done() <-chan struct{} {
+	close(m.waitingOnDone)
+
+	return m.Context.Done()
+}
+
+func newMockContext(tb testing.TB) (*mockContext, context.CancelFunc) {
+	tb.Helper()
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	return &mockContext{Context: ctx, waitingOnDone: make(chan struct{})}, cancel
+}
+
 func TestJoe_SubscribePublish(t *testing.T) {
-	t.SkipNow()
+	t.Parallel()
 
 	rp := &mockReplayProvider{errGC: errors.New("")}
 	j := sse.NewJoe(sse.JoeConfig{
 		ReplayProvider: rp,
 	})
+	defer j.Stop()
 
-	ctx, cancel := context.WithCancel(context.Background())
+	ctx, cancel := newMockContext(t)
 	defer cancel()
 
 	sub := subscribe(t, j, ctx)
-	time.Sleep(time.Millisecond * 10)
-
+	<-ctx.waitingOnDone
 	require.NoError(t, j.Publish(msg(t, "hello", "", 0, sse.DefaultTopic)))
-	time.Sleep(time.Millisecond * 10)
 	cancel()
 	require.NoError(t, j.Publish(msg(t, "world", "", 0, sse.DefaultTopic)))
-	msgs, ok := <-sub
+	msgs := <-sub
 	require.Equal(t, "data: hello\n\n", msgs[0].String())
 
-	sub2 := subscribe(t, j, ctx)
+	ctx2, cancel2 := newMockContext(t)
+	defer cancel2()
+
+	sub2 := subscribe(t, j, ctx2)
+	<-ctx2.waitingOnDone
 
 	require.NoError(t, j.Stop())
-	_, ok = <-sub2
-	require.False(t, ok)
-	require.Equal(t, 2, rp.callsPut)
-	require.Equal(t, 2, rp.callsReplay)
+	msgs = <-sub2
+	require.Zero(t, msgs, "unexpected messages received")
+	require.Equal(t, 2, rp.callsPut, "invalid put calls")
+	require.Equal(t, 2, rp.callsReplay, "invalid replay calls")
+}
+
+func TestJoe_Subscribe_multipleTopics(t *testing.T) {
+	t.Parallel()
+
+	j := sse.NewJoe()
+	defer j.Stop()
+
+	ctx, cancel := newMockContext(t)
+	defer cancel()
+
+	sub := subscribe(t, j, ctx, sse.DefaultTopic, "another topic")
+	<-ctx.waitingOnDone
+
+	_ = j.Publish(msg(t, "hello", "", 0, sse.DefaultTopic))
+	_ = j.Publish(msg(t, "world", "", 0, "another topic"))
+
+	_ = j.Stop()
+
+	msgs := <-sub
+
+	expected := `data: hello
+
+data: world
+
+`
+	require.Equal(t, expected, msgs[0].String()+msgs[1].String(), "unexpected data received")
+}
+
+func TestJoe_errors(t *testing.T) {
+	t.Parallel()
+
+	j := sse.NewJoe(sse.JoeConfig{
+		ReplayProvider: sse.NewFiniteReplayProvider(1),
+	})
+	defer j.Stop()
+
+	_ = j.Publish(msg(t, "hello", "0", 0, sse.DefaultTopic))
+	_ = j.Publish(msg(t, "hello", "1", 0, sse.DefaultTopic))
+
+	expectedErr := errors.New("")
+	var called int
+	cb := func(_ *sse.Message) error {
+		called++
+		return expectedErr
+	}
+
+	err := j.Subscribe(context.Background(), sse.Subscription{
+		Callback:    cb,
+		LastEventID: sse.MustEventID("0"),
+		Topics:      []string{sse.DefaultTopic},
+	})
+	require.Equal(t, expectedErr, err, "error not received from replay")
+
+	_ = j.Publish(msg(t, "world", "2", 0, sse.DefaultTopic))
+
+	require.Equal(t, 1, called, "callback was called after subscribe returned")
+
+	called = 0
+	ctx, cancel := newMockContext(t)
+	defer cancel()
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+
+		<-ctx.waitingOnDone
+
+		_ = j.Publish(msg(t, "", "3", 0, sse.DefaultTopic))
+		_ = j.Publish(msg(t, "", "4", 0, sse.DefaultTopic))
+	}()
+
+	err = j.Subscribe(ctx, sse.Subscription{Callback: cb, Topics: []string{sse.DefaultTopic}})
+	require.Equal(t, expectedErr, err, "error not received from send")
+	require.Equal(t, 1, called, "callback was called after subscribe returned")
+
+	<-done
 }
 
 func TestJoe_GCInterval(t *testing.T) {
+	t.Parallel()
+
 	rp := &mockReplayProvider{}
 	interval := time.Millisecond * 6
 	j := sse.NewJoe(sse.JoeConfig{
