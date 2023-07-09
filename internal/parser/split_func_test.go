6c3b19072dd6d0ca07c0c1191e113829f8fc427e
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2023-07-10 01:10:32 +0300

    Unify parser split func test cases

--- internal/parser/split_func_test.go
+++ internal/parser/split_func_test.go
@@ -10,57 +10,58 @@ import (
 func TestSplitFunc(t *testing.T) {
 	t.Parallel()
 
-	text := "\xEF\xBB\xBFmama mea e super\nce genial\nsincer n-am ce sa zic\r\n\r\n\nmama tata bunica bunicul\nsarmale\r\n\r\r\naualeu\nce taraboi"
-	r := strings.NewReader(text)
-	s := bufio.NewScanner(r)
-	s.Split(newSplitFunc())
-
-	expected := []string{
-		"mama mea e super\nce genial\nsincer n-am ce sa zic\r\n\r\n",
-		"mama tata bunica bunicul\nsarmale\r\n\r",
-		"aualeu\nce taraboi",
+	type testCase struct {
+		name     string
+		input    string
+		expected []string
 	}
-	tokens := make([]string, 0, len(expected))
-
-	for s.Scan() {
-		tokens = append(tokens, s.Text())
-	}
-
-	if s.Err() != nil {
-		t.Fatalf("an error occurred: %v", s.Err())
-	}
-
-	if !reflect.DeepEqual(tokens, expected) {
-		t.Fatalf("wrong tokens:\nreceived: %#v\nexpected: %#v", tokens, expected)
-	}
-}
-
-func TestSplitFuncWithLongLine(t *testing.T) {
-	t.Parallel()
 
 	longString := strings.Repeat("abcdef\rghijklmn\nopqrstu\r\nvwxyz", 193)
-	text := longString + "\n\n" + longString + "\r\r" + longString + "\r\n\r\n" + longString
-	r := strings.NewReader(text)
-	s := bufio.NewScanner(r)
-	s.Split(newSplitFunc())
-
-	expected := []string{
-		longString + "\n\n",
-		longString + "\r\r",
-		longString + "\r\n\r\n",
-		longString,
+	testCases := []testCase{
+		{
+			name:  "Short sample with BOM",
+			input: "\xEF\xBB\xBFmama mea e super\nce genial\nsincer n-am ce sa zic\r\n\r\n\nmama tata bunica bunicul\nsarmale\r\n\r\r\naualeu\nce taraboi",
+			expected: []string{
+				"mama mea e super\nce genial\nsincer n-am ce sa zic\r\n\r\n",
+				"mama tata bunica bunicul\nsarmale\r\n\r",
+				"aualeu\nce taraboi",
+			},
+		},
+		{
+			name:  "Long sample",
+			input: longString + "\n\n" + longString + "\r\r" + longString + "\r\n\r\n" + longString,
+			expected: []string{
+				longString + "\n\n",
+				longString + "\r\r",
+				longString + "\r\n\r\n",
+				longString,
+			},
+		},
 	}
-	tokens := make([]string, 0, len(expected))
 
-	for s.Scan() {
-		tokens = append(tokens, s.Text())
-	}
+	for _, tc := range testCases {
+		tc := tc
 
-	if s.Err() != nil {
-		t.Fatalf("an error occurred: %v", s.Err())
-	}
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
 
-	if !reflect.DeepEqual(tokens, expected) {
-		t.Fatalf("wrong tokens:\nreceived: %#v\nexpected: %#v", tokens, expected)
+			r := strings.NewReader(tc.input)
+			s := bufio.NewScanner(r)
+			s.Split(newSplitFunc())
+
+			tokens := make([]string, 0, len(tc.expected))
+
+			for s.Scan() {
+				tokens = append(tokens, s.Text())
+			}
+
+			if s.Err() != nil {
+				t.Fatalf("an error occurred: %v", s.Err())
+			}
+
+			if !reflect.DeepEqual(tokens, tc.expected) {
+				t.Fatalf("wrong tokens:\nreceived: %#v\nexpected: %#v", tokens, tc.expected)
+			}
+		})
 	}
 }
